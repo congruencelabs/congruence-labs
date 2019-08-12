3748592631eac2d04f33d66d518d2242be9f26b4
Aditya Pratap Singh <adisinghrajput@gmail.com>
2019-08-12 23:55:26 +0200

    feat: Update pending icons

--- src/components/tech.js
+++ src/components/tech.js
@@ -13,15 +13,15 @@ import K8SIcon from "../images/svg/icon-kubernetes.inline.svg";
 
 const SupportedTech = () => {
   const icons = [
-    <VueIcon width={100}/>,
-    <AngularIcon width={100}/>,
-    <GraphqlIcon width={100}/>,
-    <NpmIcon width={100}/>,
-    <ReactIcon width={100}/>,
+    <AngularIcon width={100} height={100}/>,
+    <GraphqlIcon width={100} height={100}/>,
+    <NpmIcon width={100} height={100}/>,
+    <ReactIcon width={100} height={100}/>,
     <K8SIcon width={150} height={80}/>,
     <MongoIcon width={150}/>,
     <AkkaIcon width={100} height={100}/>,
     <ScalaIcon width={150}/>,
+    <VueIcon width={100}/>,
   ];
   return (
     <div className={'tech-container'}>
