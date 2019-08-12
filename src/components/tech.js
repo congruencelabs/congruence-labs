29410b37bb40fab38a51f2983585834f45f119b5
Aditya Pratap Singh <adisinghrajput@gmail.com>
2019-08-13 00:52:19 +0200

    feat: Add more icons (#4)
    * feat: Add more icons
    
    * feat: Add grayscale and hover on images

--- src/components/tech.js
+++ src/components/tech.js
@@ -1,26 +1,47 @@
 import React from "react"
 import "./tech.css";
 
-import AngularIcon from "../images/svg/icon-angular.inline.svg";
-import GraphqlIcon from "../images/svg/icon-graphql.inline.svg";
-import MongoIcon from "../images/svg/icon-mongo.inline.svg";
-import NpmIcon from "../images/svg/icon-npm.inline.svg";
-import ReactIcon from "../images/svg/icon-react.inline.svg";
-import VueIcon from "../images/svg/icon-vue.inline.svg";
-import ScalaIcon from "../images/svg/icon-scala.inline.svg";
-import AkkaIcon from "../images/svg/icon-akka.inline.svg";
-import K8SIcon from "../images/svg/icon-kubernetes.inline.svg";
+import AkkaIcon from "../images/svg/akka.svg";
+import AngularIcon from "../images/svg/angular.svg";
+import AwsIcon from "../images/svg/aws.svg";
+import CassandraIcon from "../images/svg/cassandra.svg";
+import GoIcon from "../images/svg/golang.svg";
+import GraphqlIcon from "../images/svg/graphql.svg";
+import JavaIcon from "../images/svg/java.svg";
+import K8SIcon from "../images/svg/kubernetes.svg";
+import MongoIcon from "../images/svg/mongo.svg";
+import NodejsIcon from "../images/svg/nodejs.svg";
+import ReactIcon from "../images/svg/reactjs.svg";
+import ScalaIcon from "../images/svg/scala.svg";
+import SpringbootIcon from "../images/svg/springboot.svg";
+import TypescriptIcon from "../images/svg/typescript.svg";
+import VueIcon from "../images/svg/vue.svg";
 
 const SupportedTech = () => {
   const icons = [
     <AngularIcon width={100} height={100}/>,
+    <JavaIcon width={100} height={100} style={{paddingLeft: '20px'}}/>,
+    <AwsIcon width={100} height={100}/>,
+    <CassandraIcon width={100} height={100}/>,
     <GraphqlIcon width={100} height={100}/>,
-    <NpmIcon width={100} height={100}/>,
+    <GoIcon width={120} height={100}/>,
+    <NodejsIcon width={140} height={100} style={{
+        paddingTop: '20px',
+        paddingLeft: '20px'
+    }}/>,
     <ReactIcon width={100} height={100}/>,
     <K8SIcon width={150} height={80}/>,
     <MongoIcon width={150}/>,
+    <SpringbootIcon width={150} style={{
+        paddingLeft: '20px'
+    }}/>,
     <AkkaIcon width={100} height={100}/>,
-    <ScalaIcon width={150}/>,
+    <ScalaIcon width={150} style={{
+        paddingLeft: '20px'
+    }}/>,
+    <TypescriptIcon width={150} style={{
+        paddingLeft: '20px'
+    }}/>,
     <VueIcon width={100}/>,
   ];
   return (
